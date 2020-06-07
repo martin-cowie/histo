9386c97e2166138ca9acd064b1808cb7f1b731e4
Martin Cowie <mcowie@pushtechnology.com>
2020-06-07 22:00:30 +0100

    1st cut.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Histo
+
+Calculate a histogram from the creation date of any number of files.
+
+## Build
+
+`cargo build`
+
+## Use
+
+`find some/dir -type f | histo`
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate chrono;
+use chrono::Timelike;
+
+use chrono::offset::Utc;
+use chrono::DateTime;
+use std::fs;
+use std::collections::BTreeMap;
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut histo: BTreeMap<DateTime<Utc>, i32> = BTreeMap::new();
+
+    let mut count = 0;
+    for line in io::stdin().lock().lines() {
+        let filename = line.unwrap();
+        consume(&filename, &mut histo);
+        count+=1;
+    }
+
+    // Iterate over histo
+    for(key, value) in histo.iter() {
+        println!("{}, {}", key.format("%F %T"), value);
+    }
+    eprintln!("consumed {} files, into {} quanta", count, histo.keys().len());
+
+}
+
+fn consume(filename: &str, histo: &mut BTreeMap<DateTime<Utc>, i32>) {
+    let mt = fs::metadata(filename).unwrap();
+    let datetime: DateTime<Utc> = mt.created().unwrap().into();
+
+    let map_key = datetime
+        .with_minute(0).unwrap()
+        .with_second(0).unwrap()
+        .with_nanosecond(0).unwrap();
+
+    let count = histo.entry(map_key).or_insert(0);
+    *count += 1;
+}
